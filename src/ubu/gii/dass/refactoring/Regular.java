4197742f3809018f11a3a58ac5fe9f28ef83e8f7
afn1001 <afn1001@alu.ubu.es>
2022-05-18 19:16:44 +0200

    Create MovieType superclass and a class for each type of movie

---
+++ src/ubu/gii/dass/refactoring/Regular.java
@@ -0,0 +1,18 @@
+package ubu.gii.dass.refactoring;
+
+public class Regular extends MovieType {
+
+	@Override
+	public double getCharge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
